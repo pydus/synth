512d1b7975c9f6e168a2d8447d964440c500f807
pydus <kristofferpudas@gmail.com>
2016-06-11 02:13:15 +0200

    it now makes noise

--- client/app.js
+++ client/app.js
@@ -1,2 +1,17 @@
 require('./thing')();
 require('./style.sass');
+
+const audioCtx   = new (window.AudioContext || window.webkitAudioContext)(),
+      oscillator = audioCtx.createOscillator(),
+      keys       = document.getElementsByClassName('key');
+
+oscillator.type = 'sawtooth';
+oscillator.start();
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('click', event => {
+    oscillator.frequency.value = 440;
+    oscillator.connect(audioCtx.destination);
+    setTimeout(() => oscillator.disconnect(), 200);
+  });
+}
