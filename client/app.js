6c964ea90ff1c583aa9c7dfd0fb621330c673001
pydus <kristofferpudas@gmail.com>
2016-06-11 02:55:58 +0200

    small improvements

--- client/app.js
+++ client/app.js
@@ -6,14 +6,18 @@ const audioCtx   = new (window.AudioContext || window.webkitAudioContext)(),
       keys       = document.getElementsByClassName('key'),
       a          = Math.pow(2, 1 / 12);
 
+const getFrequency = semitone => {
+  return 440 * Math.pow(a, semitone);
+};
+
 oscillator.type = 'sawtooth';
 oscillator.start();
 
 for (var i = 0; i < keys.length; i++) {
   const key = keys[i];
   key.addEventListener('click', event => {
-    const n = key.getAttribute('id');
-    oscillator.frequency.value = 440 * Math.pow(a, n);
+    const semitone = key.getAttribute('id');
+    oscillator.frequency.value = getFrequency(semitone);
     oscillator.connect(audioCtx.destination);
     setTimeout(() => oscillator.disconnect(), 400);
   });
