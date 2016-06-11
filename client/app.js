c4a54b735c113707316deca4ff94f20c56ab74b9
pydus <kristofferpudas@gmail.com>
2016-06-11 02:33:32 +0200

    it now plays the correct frequencies

--- client/app.js
+++ client/app.js
@@ -3,15 +3,18 @@ require('./style.sass');
 
 const audioCtx   = new (window.AudioContext || window.webkitAudioContext)(),
       oscillator = audioCtx.createOscillator(),
-      keys       = document.getElementsByClassName('key');
+      keys       = document.getElementsByClassName('key'),
+      a          = Math.pow(2, 1 / 12);
 
 oscillator.type = 'sawtooth';
 oscillator.start();
 
 for (var i = 0; i < keys.length; i++) {
-  keys[i].addEventListener('click', event => {
-    oscillator.frequency.value = 440;
+  const key = keys[i];
+  key.addEventListener('click', event => {
+    const n = key.getAttribute('id');
+    oscillator.frequency.value = 440 * Math.pow(a, n);
     oscillator.connect(audioCtx.destination);
-    setTimeout(() => oscillator.disconnect(), 200);
+    setTimeout(() => oscillator.disconnect(), 400);
   });
 }
