bde13c08e298d4b0ef3c2eb385cbbb3dc92e8e6b
pydus <kristofferpudas@gmail.com>
2016-06-11 21:56:39 +0200

    moved synth js to new file, funky colors, new slider look

--- client/panel.js
+++ client/panel.js
@@ -4,19 +4,19 @@ var panel = {
   waveform: 'sine'
 };
 
+var quad, button;
 for (var i = 0; i < quads.length; i++) {
-  var quad = quads[i];
+  quad = quads[i];
 
   for (var j = 0; j < 4; j++) {
-    var button = quad.children[j];
+    button = quad.children[j];
 
-    if (button.id === 'sine')
+    if (button.id === panel.waveform)
       button.classList.add('pressed');
 
     button.addEventListener('click', function(event) {
       for (var k = 0; k < 4; k++)
         quad.children[k].classList.remove('pressed');
-
       this.classList.add('pressed');
       panel.waveform = this.id;
     });
---
+++ client/synth.js
@@ -0,0 +1,56 @@
+const panel      = require('./panel'),
+      audioCtx   = new (window.AudioContext || window.webkitAudioContext)(),
+      oscillator = audioCtx.createOscillator(),
+      keys       = document.getElementsByClassName('key'),
+      a          = Math.pow(2, 1 / 12);
+
+var isPressingKey = false;
+
+const getFrequency = semitone => {
+  return 440 * Math.pow(a, semitone);
+};
+
+const play = semitone => {
+  oscillator.frequency.value = getFrequency(semitone);
+  oscillator.type = panel.waveform;
+  oscillator.connect(audioCtx.destination);
+};
+
+const initializeKeys = () => {
+  for (var i = 0; i < keys.length; i++) {
+    const key      = keys[i],
+          semitone = key.getAttribute('id');
+
+    key.addEventListener('mousedown', event => {
+      isPressingKey = true;
+      play(semitone);
+      key.classList.remove('not-active');
+    });
+
+    key.addEventListener('mouseover', event => {
+      if (isPressingKey) {
+        play(semitone);
+        key.classList.add('pressed');
+        key.classList.remove('not-active');
+      }
+    });
+
+    key.addEventListener('mouseout', event => {
+      oscillator.disconnect();
+      key.classList.add('not-active');
+      key.classList.remove('pressed');
+    });
+
+    key.addEventListener('mouseup', event => {
+      key.classList.remove('pressed');
+    });
+  }
+
+  document.addEventListener('mouseup', event => {
+    oscillator.disconnect();
+    isPressingKey = false;
+  });
+};
+
+initializeKeys();
+oscillator.start();
