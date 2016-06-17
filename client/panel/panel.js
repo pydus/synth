181e32046363e51354b4353848e6414437dc0883
pydus <kristofferpudas@gmail.com>
2016-06-17 22:30:09 +0200

    made matrix knobs controllable

--- client/panel/panel.js
+++ client/panel/panel.js
@@ -6,13 +6,13 @@ const OscillatorUnit = require('./oscillator-unit'),
       MAX_AMP_GAIN   = 0.15,
       MAX_OSC_GAIN   = 0.5;
 
-var headings  = document.querySelectorAll('.oscillator .main h1'),
-    quads     = document.getElementsByClassName('quadbutton'),
-    knobs     = document.querySelectorAll('.oscillator .main .knob'),
-    tubes     = document.getElementsByClassName('tube'),
-    verticals = document.getElementsByClassName('vertical'),
-    ampKnobs  = document.querySelector('.amp .knobs'),
-    matrix    = document.querySelector('.matrix');
+var headings    = document.querySelectorAll('.oscillator .main h1'),
+    quads       = document.getElementsByClassName('quadbutton'),
+    knobs       = document.querySelectorAll('.oscillator .main .knob'),
+    tubes       = document.getElementsByClassName('tube'),
+    verticals   = document.getElementsByClassName('vertical'),
+    ampKnobs    = document.querySelector('.amp .knobs'),
+    matrixKnobs = document.querySelectorAll('.matrix .knob');
 
 var nOscillators    = 6,
     oscillatorUnits = [];
@@ -25,6 +25,7 @@ var ampGain     = new Knob(ampKnobs.children[2], 0, '%', 0, 1),
     ampEnvelope = new EnvelopeUnit(attack, decay, sustain, release);
 
 var masterGain = new Tube(tubes[0], 0.8 * MAX_AMP_GAIN, '%', 0, MAX_AMP_GAIN);
+var matrix = [];
 
 const initializeHeadings = () => {
   for (var i = 0; i < headings.length; i++) {
@@ -33,41 +34,54 @@ const initializeHeadings = () => {
   }
 };
 
+const initializeMatrix = () => {
+  for (var i = 0; i < Math.pow(nOscillators, 2); i++) {
+    var knob = new Knob(matrixKnobs[i], 0, '%', 0, 1);
+    knob.initialize();
+    matrix.push(knob);
+  }
+};
+
+const initializeOscillators = () => {
+  for (var i = 0; i < nOscillators; i++) {
+    var waveform = new Quad(quads[i], 'sine'),
+        detune   = new Knob(knobs[i * 3], 0, 'cents', 0, 1200, true),
+        cutoff   = new Knob(knobs[i * 3 + 1], 8000, 'Hz', 0, 10000),
+        gain     = new Knob(knobs[i * 3 + 2], 0.5 * MAX_OSC_GAIN, '%', 0, MAX_OSC_GAIN),
+        attack   = new Knob(verticals[i].children[1], 0, 's', 0, 1),
+        decay    = new Knob(verticals[i].children[3], 0.5, 's', 0, 1),
+        sustain  = new Knob(verticals[i].children[5], 0.5 * MAX_OSC_GAIN, '%', 0, MAX_OSC_GAIN),
+        release  = new Knob(verticals[i].children[7], 0, 's', 0, 1),
+        envUnit  = new EnvelopeUnit(attack, decay, sustain, release),
+        osc      = new OscillatorUnit(waveform, detune, cutoff, gain, envUnit);
+
+    if (i > 3)
+      osc.running = false;
+
+    panel[`osc${i + 1}`] = osc;
+    osc.initialize();
+    oscillatorUnits.push(osc);
+  }
+};
+
 const initialize = () => {
-  oscillatorUnits.forEach(osc => osc.initialize());
+  initializeOscillators();
   ampEnvelope.initialize();
   masterGain.initialize();
   ampGain.initialize();
   initializeHeadings();
+  initializeMatrix();
 };
 
 var panel = {
   initialize: initialize,
   nOscillators: nOscillators,
+  matrix: matrix,
   amp: {
     gain: ampGain,
     envelope: ampEnvelope
   },
   gain: masterGain
 };
-
-for (var i = 0; i < nOscillators; i++) {
-  var waveform = new Quad(quads[i], 'sine'),
-      detune   = new Knob(knobs[i * 3], 0, 'cents', 0, 1200, true),
-      cutoff   = new Knob(knobs[i * 3 + 1], 8000, 'Hz', 0, 10000),
-      gain     = new Knob(knobs[i * 3 + 2], 0.5 * MAX_OSC_GAIN, '%', 0, MAX_OSC_GAIN),
-      attack   = new Knob(verticals[i].children[1], 0, 's', 0, 1),
-      decay    = new Knob(verticals[i].children[3], 0.5, 's', 0, 1),
-      sustain  = new Knob(verticals[i].children[5], 0.5 * MAX_OSC_GAIN, '%', 0, MAX_OSC_GAIN),
-      release  = new Knob(verticals[i].children[7], 0, 's', 0, 1),
-      envUnit  = new EnvelopeUnit(attack, decay, sustain, release),
-      osc      = new OscillatorUnit(waveform, detune, cutoff, gain, envUnit);
-
-  if (i > 3)
-    osc.running = false;
-
-  panel[`osc${i + 1}`] = osc;
-  oscillatorUnits.push(osc);
-}
 
 module.exports = panel;
